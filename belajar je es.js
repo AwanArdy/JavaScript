9969cd9dafbdcc3a8e385b50a006990f285ef993
AwanArdy <muhammadawan46@gmail.com>
2023-12-12 16:34:35 +0800

    belajar

--- belajar je es.js
+++ belajar je es.js
@@ -563,7 +563,7 @@ function testElse(val) {
 
 testElse(4);
 
-// introducing Else If Statements
+// Introducing Else If Statements
 function testElseIf(val) {
     if (val > 10) {
         return "Greater than 10";
@@ -575,3 +575,184 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
+// Logical Order in If Else Statements
+function orderMyLogical(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+
+orderMyLogical(7);
+
+// Chaining If Else Statements
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "large";
+    } else if (num >= 20) {
+        return "Huge"
+    } else {
+        return "Change Me";
+    }
+}
+
+testSize(7);
+
+// Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return "Hole-in-one!";
+    } else if (strokes <= par - 2) {
+        return "Eagle";
+    } else if (strokes == par - 1) {
+        return "Birdie";
+    } else if (strokes == par) {
+        return "Par";
+    } else if (strokes == par + 1) {
+        return "Bogey";
+    } else if (strokes == par + 2) {
+        return "Double Bogey";
+    } else if (strokes >= par + 3) {
+        return "Go Home!";
+    } else {
+        return "Change Me";
+    }
+}
+
+golfScore(5, 4)
+
+// Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+    let answer = "";
+
+    switch (val) {
+        case 1:
+            return "alpha";
+            break;
+        case 2:
+            return "beta";
+            break;
+        case 3:
+            return "gamma";
+            break;
+        case 4:
+            return "delta";
+            break;
+    }
+
+    return answer;
+}
+
+caseInSwitch(1);
+
+// Adding a Default Option in Switch Statements
+function siwtchOfStuff(val) {
+    let answer = "";
+
+    switch (val) {
+        case a:
+            answer = "apple";
+            break;
+        case b:
+            answer = "bird";
+            break;
+        case c:
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+    }
+
+    return answer;
+}
+
+siwtchOfStuff(1);
+
+// Multiple identical Options in Switch Statements
+function sequentialSize(val) {
+    let answer = "";
+
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            return "Low";
+        case 4:
+        case 5:
+        case 6:
+            return "Mid";
+        case 7:
+        case 8:
+        case 9:
+            return "High";
+    }
+
+    return answer;
+}
+
+sequentialSize(1);
+
+// Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+    let answer = "";
+
+    switch (val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+    }
+
+    return answer;
+}
+
+// Returning Boolean Values from Functions
+function isLess(a, b) {
+    return a <= b;
+}
+
+isLess(10, 15);
+
+// Return Early Pattern for Functions
+function abTest(a, b) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+// Counting Cards
+let count = 0;
+
+function cc(card) {
+
+
+    return "Change Me"
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
